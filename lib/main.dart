0d16dee543bb1945f0c6afcebb090a6254286af7
Malik Aziz Ali <malikazizali86@gmail.com>
2023-12-26 09:18:10 +0700

    first commit : add firebase authentication

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lapor_book/firebase_options.dart';
+import 'package:lapor_book/pages/add_form_page.dart';
+import 'package:lapor_book/pages/dashboard_page.dart';
+import 'package:lapor_book/pages/login_page.dart';
+import 'package:lapor_book/pages/register_page.dart';
+import 'package:lapor_book/pages/splash_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    title: 'Lapor Book',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      '/login': (context) => const LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      '/dashboard': (context) => const DashboardPage(),
+      '/add': (context) => const AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
+}
